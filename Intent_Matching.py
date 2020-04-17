56ec9ffbb38d03f1a8446a9c79ca588990bfdf97
anushanramesh <hpanushan@gmail.com>
2020-04-17 11:58:34 +0530

    Voice recognition API connecting script is added

---
+++ Intent_Matching.py
@@ -0,0 +1,8 @@
+
+def intent_matching():
+    # Keywords
+    recharge_issue = ['recharge_issue','topup', 'balance','recharge','recharge card','credit','credit amount']
+    service_failure = ['service failure', 'service','subscribe', 'unsubscribe']                             # Subscribe/Unsubscribe
+    network_faiulre = ['network_failure','signal strength','disconnect','disconnects', 'signal', 'network', 'coverage', 
+                        'network coverage']
+
