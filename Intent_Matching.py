067f0727fdf0e4a5e513274977b111fa95b7fb4d
anushanramesh <hpanushan@gmail.com>
2020-04-18 22:53:00 +0530

    Added the part of counting keywords for each intent and identify correct intent for text

--- Intent_Matching.py
+++ Intent_Matching.py
@@ -7,6 +7,13 @@ def intent_matching(text):
     # convert text into lowercase
     text = text.lower()
 
+    # Counts
+    counts = {
+        'recharge_issue' : 0,
+        'service_failure' : 0,
+        'network_faiulre' : 0
+    }
+
     # Keywords
     # Monthly,daily,charged?
     recharge_issue = ['balanc','topup','credit','recharg','reload']
@@ -22,14 +29,13 @@ def intent_matching(text):
     stop_words = set(stopwords.words('english')) 
 
     filtered_tokens = [w for w in unique_tokens if not w in stop_words] 
-    print(filtered_tokens)
 
     ## Converting each tokens to root format
     # Stemming - (originated-->origin , learning-->learn)
     # Lemmatization - (skills-->skill) (Handles same meaning tokens)
     
     root_tokens = []
-    print("")
+   
     for token in filtered_tokens:
         stemmed = stemming(token)
         lemmatized = lemmatization(token)
@@ -40,15 +46,43 @@ def intent_matching(text):
         else:
             root_tokens.append(stemmed)
 
-    return root_tokens
+    # Counting keywords for each intent
+    for token in root_tokens:
+        if token in recharge_issue:
+            counts['recharge_issue'] += 1
+        
+        elif token in service_failure:
+            counts['service_failure'] += 1
+
+        elif token in network_faiulre:
+            counts['network_faiulre'] += 1
+
+        else: pass
+
+
+    # Sorting dictionary by its value
+    counts = {k: v for k, v in sorted(counts.items(), key=lambda item: item[1], reverse=True)}
+    
+    # Selecting the case with maximum value
+    max_count =  max(counts.values())   
+
+    # Calculating the threshold
+    threshold = max_count * 0.5        
+    
+    # Checking the cases with max value
+    issues = [k for k,v in counts.items() if v >= threshold]
+
+    return issues
+
 
 
 text = """
 
 
-hello I am calling to know if my recharge balance
- thank you but also is there another way that I can check all these balances rather than always calling is there anything I should know that I can check balances
- okay thank you
+hello hello so my network has been having some issues but I subscribe for a service also few months a few weeks back actually a month ago and but I expected it to work but then after the first few days it doesn't work I don't get any notifications regards to that can you please look into this yeah my my number is 07762 411 that is correct yes can you please check on it online
+ yeah the network issue is very common in this area but yeah just I need this subscription sorted for now because God that is the most easiest thing that affects at this time because you know network is much more Hardware rather than software so I'm okay thank you thank you
+
+
 
 
 """
