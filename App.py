b1d56e5a62102ed9a74ab841e78e0316d3183310
anushanramesh <anushan@globalwavenet.com>
2020-04-30 14:35:17 +0530

    New routing method is added to flask app

--- App.py
+++ App.py
@@ -27,8 +27,29 @@ class Intent(Resource):
 
         return 1, 201
 
+class Test(Resource):
+    def get(self):
+        # Get the current intents
+        db_obj = MySQL_Intents_Keywords('146.148.85.146','root','Omnibis.1234','speech')
+        intents = db_obj.get_column_names('keywords')
+        return {"keywords":intents}, 200
+
+    def post(self):
+        # Getting Post data
+        post_json = request.get_json()
+
+        # Convert JSON into Dictionary
+        post_dict = json.loads(post_json)
+
+        # Key values of post data
+        user_input = post_dict['user_input']
+        options = post_dict['options']
+
+        return 1, 201
+
 # Routes
 api.add_resource(Intent,'/intent')
+api.add_resource(Test,'/test')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port='5000',debug=True)
