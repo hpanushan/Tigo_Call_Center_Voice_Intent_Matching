5272f7b00b968b6e2f0cad0221f208631d20354d
anushanramesh <hpanushan@gmail.com>
2020-05-06 17:40:03 +0530

    Front end side user data are passed to backend application for new intent and update intent

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,28 @@ def new():
 @app.route('/execute')
 def execute():
     return render_template('execute.html')
+
+@app.route('/new_submit', methods=['POST'])
+def new_submit():
+    intent_name = request.form['intentname']
+    keywords = request.form['keywords']
+
+    # Convert the keywords string into a list of keywords
+    keywords = keywords.split(',')
+    print(intent_name,keywords)
+
+    return render_template('new.html')
+
+@app.route('/update_submit', methods=['POST'])
+def update_submit():
+    intent_name = request.form['intentname']
+    keywords = request.form['keywords']
+    
+    # Convert the keywords string into a list of keywords
+    keywords = keywords.split(',')
+    print(intent_name,keywords)
+
+    return render_template('update.html')
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1",port="5000",debug=True)
