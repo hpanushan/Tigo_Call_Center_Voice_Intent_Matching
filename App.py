e05cd0ce192b610db2dc75544c10eb54a869ced2
anushanramesh <hpanushan@gmail.com>
2020-05-07 13:25:13 +0530

    Modified the scripts to fetch file names in test voice clips and voice clips folder to update dual select box

--- App.py
+++ App.py
@@ -2,36 +2,45 @@ from flask import Flask, render_template, url_for, request
 
 from MySQL_DB.MySQL_Intents_Keywords import MySQL_Intents_Keywords
 from Create_Table_Name import create_table_name
+from Get_File_Names import get_file_names
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    #db_obj = MySQL_Intents_Keywords('146.148.85.146','root','Omnibis.1234','speech')
-    #intent_names = db_obj.get_table_names()
-    intent_names = ['add','adada','dadd']
+    db_obj = MySQL_Intents_Keywords('146.148.85.146','root','Omnibis.1234','speech')
+    intent_names = db_obj.get_table_names()
+    #intent_names = ['add','adada','dadd']
     return render_template('index.html',intents=intent_names)
 
 @app.route('/update')
 def update():
-    #db_obj = MySQL_Intents_Keywords('146.148.85.146','root','Omnibis.1234','speech')
-    #intent_names = db_obj.get_table_names()
-    intent_names = ['add','adada','dadd']
+    db_obj = MySQL_Intents_Keywords('146.148.85.146','root','Omnibis.1234','speech')
+    intent_names = db_obj.get_table_names()
+    #intent_names = ['add','adada','dadd']
     return render_template('update.html',intents=intent_names)
 
 @app.route('/new')
 def new():
-    #db_obj = MySQL_Intents_Keywords('146.148.85.146','root','Omnibis.1234','speech')
-    #intent_names = db_obj.get_table_names()
-    intent_names = ['add','adada','dadd']
+    db_obj = MySQL_Intents_Keywords('146.148.85.146','root','Omnibis.1234','speech')
+    intent_names = db_obj.get_table_names()
+    #intent_names = ['add','adada','dadd']
     return render_template('new.html',intents=intent_names)
 
 @app.route('/execute')
 def execute():
-    #db_obj = MySQL_Intents_Keywords('146.148.85.146','root','Omnibis.1234','speech')
-    #intent_names = db_obj.get_table_names()
-    intent_names = ['add','adada','dadd']
-    return render_template('execute.html',intents=intent_names)
+    # Attributes
+    path1 = "/opt/test-voice-clips"
+    path2 = "/opt/voice-clips"
+
+    db_obj = MySQL_Intents_Keywords('146.148.85.146','root','Omnibis.1234','speech')
+    intent_names = db_obj.get_table_names()
+    #intent_names = ['add','adada','dadd']
+
+    test_voice_clips = get_file_names(path1)
+    voice_clips = get_file_names(path2)
+
+    return render_template('execute.html',intents=intent_names, test_voice_clips=test_voice_clips, voice_clips=voice_clips)
 
 @app.route('/new_submit', methods=['POST'])
 def new_submit():
@@ -83,4 +92,4 @@ def update_submit():
     return render_template('update.html')
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",port="5000",debug=True)
+    app.run(host="0.0.0.0",port="5000",debug=True)
